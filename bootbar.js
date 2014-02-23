c9ceb6af9fcc5094ab710c1bac514d3caa0378fd
Scott Blevins <sblevins@gmail.com>
2014-02-23 10:46:21 -0600

    More tests, changed method structure

--- bootbar.js
+++ bootbar.js
@@ -1,57 +1,88 @@
 (function($){
     "use strict";
     $.extend($, {
-        bootbar: function(message, options){
-            var alertTypes = ["info", "warning", "error", "success"];
-            var defaults = {
-                    barType: alertTypes[0],  // info
-                    autoDismiss: false,      // Don't automatically dismiss the bar.
-                    dismissTimeout: 3000,    // 3 Seconds
-                    dismissEffect: "slide",  // Slide away: (slide, fade)
-                    dismissSpeed: "slow",    // Dismiss speed: (slow, fast)
-                    onDraw: null,            // onDraw callback
-                    onDismiss: null          // onDismiss callback
-            };
-            var settings = $.extend({}, defaults, options || {});
-            var template = $("<div class=\"alert alert-dismissable alert-messages\">" +
-                                "<button type=\"button\" class=\"close\" aria-hidden=\"true\">&times;</button>" +
-                             "</div>");
+        bootbar: {
+            info: function(message, options) {
+                options = options === undefined ? {} : options;
+                options.barType = "info";
+                this.show(message, options);
+            },
+            warning: function(message, options) {
+                options = options === undefined ? {} : options;
+                options.barType = "warning";
+                this.show(message, options);
+            },
+            danger: function(message, options) {
+                options = options === undefined ? {} : options;
+                options.barType = "danger";
+                this.show(message, options);
+            },
+            success: function(message, options) {
+                options = options === undefined ? {} : options;
+                options.barType = "success";
+                this.show(message, options);
+            },
+            show: function(message, options) {
+                var alertTypes = ["info", "warning", "danger", "success"];
 
-            template.addClass("alert-" + settings.barType);
-            template.append(message);
+                var defaults = {
+                        autoDismiss: false,      // Don't automatically dismiss the bar.
+                        autoLinkClass: true,     // onDraw callback
+                        barType: alertTypes[0],  // info
+                        dismissTimeout: 3000,    // 3 Seconds
+                        dismissEffect: "slide",  // Slide away: (slide, fade)
+                        dismissSpeed: "fast",    // Dismiss speed: (slow, fast)
+                        onDraw: null,            // onDraw callback
+                        onDismiss: null          // onDismiss callback
+                };
 
-            function triggerClick() {
-                $(template).find(".close").trigger("click");
-            }
+                var settings = $.extend({}, defaults, options || {});
 
-            $("body").prepend(template).each(function() {
-                if ($.isFunction(settings.onDraw)) {
-                    settings.onDraw.call(this);
+                var template = $("<div class=\"alert alert-dismissable alert-messages\">" +
+                                    "<button type=\"button\" class=\"close\" aria-hidden=\"true\">&times;</button>" +
+                                "</div>");
+
+                template.addClass("alert-" + settings.barType);
+                template.append(message);
+
+                if (settings.autoLinkClass) {
+                    template.find("a").addClass("alert-link");
                 }
-            });
 
-            if (settings.autoDismiss) {
-                setTimeout(triggerClick, settings.dismissTimeout);
-            }
-
-            $(template).find(".close").unbind("click");
-
-            $(template.find(".close")).on("click", function() {
-                if (settings.dismissEffect === "slide") {
-                    $(template).slideUp(settings.dismissSpeed, function() {
-                        if ($.isFunction(settings.onDismiss)) {
-                            settings.onDismiss.call(this);
-                        }
-                    });
-                } else {
-                    $(template).fadeOut(settings.dismissSpeed, function() {
-                        if ($.isFunction(settings.onDismiss)) {
-                            settings.onDismiss.call(this);
-                        }
-                    });
+                function triggerClick() {
+                    $(template).find(".close").trigger("click");
                 }
-            });
-            return this;
+
+                $("body").prepend(template).each(function() {
+                    if ($.isFunction(settings.onDraw)) {
+                        settings.onDraw.call(this);
+                    }
+                });
+
+                if (settings.autoDismiss) {
+                    setTimeout(triggerClick, settings.dismissTimeout);
+                }
+
+                $(template).find(".close").unbind("click");
+
+                $(template.find(".close")).on("click", function() {
+                    if (settings.dismissEffect === "slide") {
+                        $(template).slideUp(settings.dismissSpeed, function() {
+                            $(template).remove();
+                            if ($.isFunction(settings.onDismiss)) {
+                                settings.onDismiss.call(this);
+                            }
+                        });
+                    } else {
+                        $(template).fadeOut(settings.dismissSpeed, function() {
+                            $(template).remove();
+                            if ($.isFunction(settings.onDismiss)) {
+                                settings.onDismiss.call(this);
+                            }
+                        });
+                    }
+                });
+            }
         }
     });
 })(window.jQuery || window.Zepto || window.$);
--- tests/bootbar.test.js
+++ tests/bootbar.test.js
@@ -1,13 +1,31 @@
 describe("Bootbar", function() {
 
     it("is attached to the jQuery object", function() {
-        expect(jQuery.bootbar).to.be.an("function");
+        expect(jQuery.bootbar).to.be.an("object");
+        expect(jQuery.bootbar.show).to.be.an("function");
+        expect(jQuery.bootbar.info).to.be.an("function");
+        expect(jQuery.bootbar.warning).to.be.an("function");
+        expect(jQuery.bootbar.danger).to.be.an("function");
+        expect(jQuery.bootbar.success).to.be.an("function");
     });
 
     it("calls the onDraw callback", function () {
         var callback = sinon.spy();
-        $.bootbar("Test", {"onDraw": callback});
+        $.bootbar.show("Test", {"onDraw": callback});
+        $(".alert-info").remove();
         assert(callback.called);
+    });
+
+    it("adds the alert-link class to links in message", function () {
+        $.bootbar.show("Test <a href=\"#\">this<\/a>");
+        assert($(".alert-info").find("a").hasClass("alert-link"));
+        $(".alert-info").remove();
+    });
+
+    it("does not add the alert-link class to links in message", function () {
+        $.bootbar.warning("Test <a href=\"#\">this<\/a>", {"autoLinkClass": false});
+        assert(!$(".alert-warning").find("a").hasClass("alert-link"));
+        $(".alert-warning").remove();
     });
 
     var clock;
@@ -31,7 +49,7 @@ describe("Bootbar", function() {
 
         throttled();
 
-        $.bootbar("Test", {"autoDismiss": true, "dismissTimeout": 100, "onDismiss": callback});
+        $.bootbar.show("Test", {"autoDismiss": true, "dismissTimeout": 100, "onDismiss": callback});
 
         clock.tick(99);
         assert(callback.notCalled);
